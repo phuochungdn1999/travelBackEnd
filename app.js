b07ec009b1fe874aac02b450385ee5a5ed01d311
Phan Thanh Huy <huy3999@gmail.com>
2020-07-25 22:59:31 +0700

    fix cors

--- app.js
+++ app.js
@@ -15,11 +15,7 @@ const searchplace= require("./routes/searchplace");
 const hotplaceRoute= require("./routes/hotplaces");
 //use route
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 app.use(userRoute);
 app.use("/places", placeRoute);
 app.use("/hotels", hotelRoute);
--- routes/hotels.js
+++ routes/hotels.js
@@ -4,6 +4,11 @@ const Hotel = require("../models/hotel");
 const auth = require("../middleware/auth");
 //token gồm Bearer + token (sau Bearer có dấu cách)
 //get all
+router.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
 router.get("/", async (req, res) => {
   try {
     const perPage = parseInt(req.query.limit || 1000)
